5bf8639b6b9ebaecc9482ce669383c5a42a26fee
John cx <cxcuixuan@gmail.com>
2015-05-15 14:40:02 +0800

    Update layer_cal.m
    use exp replace expm for 60s faster in laptap

--- layer_cal.m
+++ layer_cal.m
@@ -1,11 +1,12 @@
-% 验证无误
+% 脩茅脰陇脦脼脦贸
 layerNum = length(eps);
 for n = 1:layerNum
     kZ = conj(sqrt(eps(n)*miu(n)-kX^2-kY^2));
     Q = (1/miu(n))*[kX*kY miu(n)*eps(n)-kX^2 ; kY^2-miu(n)*eps(n) -kX*kY]; W = eye(length(Q));
     Omega = 1i*kZ*W ; V = Q/Omega; 
     A = W+V\V_h ; B = W - V\V_h ;
-    X = expm(-Omega*k0*thickness(n));
+    % X = expm(-Omega*k0*thickness(n));
+    X = diag(exp(-diag(Omega)*k0*thickness(n)));
     S11_d = (A - X*B/A*X*B)\(X*B/A*X*A-B);
     S12_d = (A - X*B/A*X*B)\X*(A-B/A*B);
     S21_d = S12_d ; S22_d = S11_d ; 
